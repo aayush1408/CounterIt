8f3675b75f145c4a523af334f70f0aae680dcf6e
aayush1408 <aayushjaiswal0114@gmail.com>
2018-08-26 17:41:57 +0530

    passport added

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const config = require('./config');
 const path = require('path');
+const bodyParser = require('body-parser');
+var passport = require('passport');
+var Strategy = require('passport-local').Strategy;
 const ejs = require('ejs');
 const app = express();
 
@@ -9,6 +12,9 @@ const app = express();
 app.set('views', path.join(__dirname, 'client'));
 app.set('view engine', 'ejs');
 
+// Setup body-parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
 
 const mongoURI = `mongodb://${config.username}:${config.password}@ds125372.mlab.com:25372/counter1408`;
 mongoose.connect(mongoURI)
@@ -22,7 +28,35 @@ const UniqueCounter = require('./models/unique_counter');
 const MonthCounter = require('./models/month_counter');
 const UniqueMonthCounter = require('./models/unique_month_counter');
 const Ip = require('./models/ip');
+const Admin = require('./models/admin');
 
+passport.use(new Strategy(
+    function (username, password, cb) {
+        Admin.findOne({ username: username }, function (err, user) {
+            if (err) { return cb(err); }
+            if (!user) { return cb(null, false); }
+            if (user.password != password) { return cb(null, false); }
+            return cb(null, user);
+        });
+    }));
+
+passport.serializeUser(function (user, cb) {
+    cb(null, user.id);
+});
+
+passport.deserializeUser(function (id, cb) {
+    Admin.findById(id, function (err, user) {
+        if (err) { return cb(err); }
+        cb(null, user);
+    });
+});
+
+app.use(require('express-session')({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Welcome page
 app.get('/', (req, res) => {
     let ip_address = req.ip;
     let date = new Date();
@@ -30,7 +64,7 @@ app.get('/', (req, res) => {
     let current_month = date.getMonth();
     Counter.findOne({ id: 1 }).then((data) => {
         console.log(data.date.getDate());
-        if (current_date > data.date.getDate()) {
+        if (current_date - data.date.getDate() != 0) {
             Counter.updateOne({ id: 1 }, { $set: { date: date } }).then((data) => {
                 console.log('Updated date', date)
             })
@@ -79,7 +113,7 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/client/index.html');
 });
 
-app.get('/counter', (req, res) => {
+app.get('/counter', require('connect-ensure-login').ensureLoggedIn(), (req, res) => {
     let final = { counter: '', month_counter: '', unique_counter: '', unique_month_counter: '' };
     Counter.findOne({ id: 1 }, (err, data) => {
         if (err) throw err;
@@ -99,7 +133,25 @@ app.get('/counter', (req, res) => {
         });
     });
 });
+app.get('/login',
+    function (req, res) {
+        res.render('login');
+    });
 
+app.post('/login',
+    passport.authenticate('local', { failureRedirect: '/login' }),
+    function (req, res) {
+        res.redirect('/counter');
+    });
+
+app.get('/logout',
+    function (req, res) {
+        req.logout();
+        res.redirect('/login');
+    });
+
+
+// Serve port
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on a port of ${PORT}`);
